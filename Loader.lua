1fc4efcd14c600228bac93a98f7db99d47caf13a
Van Gogh <bonisrealfrfr@gmail.com>
2024-10-05 10:09:37 -0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+local placeId = game.PlaceId
+if placeId == 14702923685 or 16335600896 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zglad-Creator/Respectful_Residents/refs/heads/main/Main.lua"))()
+elseif placeId == 13950108612 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zglad-Creator/Respectful_Residents/refs/heads/main/Lobby.lua"))()
+else
+    print("Not Supported :(")
+end
