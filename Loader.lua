70327235cd62b4cf587d084e1e65bc277b8adf97
Van Gogh <bonisrealfrfr@gmail.com>
2024-10-06 11:44:52 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,22 @@
 local placeId = game.PlaceId
-if placeId == 14702923685 or 16335600896 then
+if placeId == 14702923685 or placeId == 16335600896 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Zglad-Creator/Respectful_Residents/refs/heads/main/Main.lua"))()
 elseif placeId == 13950108612 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Zglad-Creator/Respectful_Residents/refs/heads/main/Lobby.lua"))()
 else
     print("Not Supported :(")
 end
+while true do
+    local screenGui = game:GetService("CoreGui"):FindFirstChild("ScreenGui")
+    if screenGui then
+        screenGui:Destroy()
+        print("lol")
+    end
+    local turtleSpyGui = game:GetService("CoreGui"):FindFirstChild("TurtleSpyGUI")
+    if turtleSpyGui then
+        turtleSpyGui:Destroy()
+        print("lol")
+    end
+    wait(0.1)
+end
+
