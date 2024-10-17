607127c8a8f2315fed8aea9a0e34800e16070a0e
Ethan Zhou <131318416+SennLynn@users.noreply.github.com>
2024-10-17 10:05:31 -0700

    Update exclude.py

--- exclude.py
+++ exclude.py
@@ -18,3 +18,12 @@ number = random.randint(1, 20)
 guess = 0
 while guess != random:
   guess = input("guess a number from 1 to 20: ")
+
+print("heads or tails")
+new_number = input("Do you think it is heads or tails, (1 is tails, 2 is heads)")
+if new_number == 1:
+  print("It was heads!")
+elif new_number == 2:
+  print("It was tails!")
+else:
+  print("input 1 or 2 nect time >:(")
