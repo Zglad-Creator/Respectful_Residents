31fe871625823d8f3adc303acdeb579c6b3f76b5
Van Gogh <bonisrealfrfr@gmail.com>
2024-10-20 15:40:24 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 local placeId = game.PlaceId
 if placeId == 14702923685 or placeId == 16335600896 then
-    loadstring(game:HttpGet("https://pastebin.com/raw/pnFuZwxk"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zglad-Creator/Respectful_Residents/refs/heads/main/Main.lua"))()
 elseif placeId == 13950108612 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Zglad-Creator/Respectful_Residents/refs/heads/main/Lobby.lua"))()
 else
