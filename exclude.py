9ce1c1ec7b5d8a8948f9601bf1fa8ea38d556b93
P4nfried <149710447+P4nfried@users.noreply.github.com>
2024-10-22 12:56:00 -0700

    Update exclude.py

--- exclude.py
+++ exclude.py
@@ -5,3 +5,4 @@ while dog:
   user_input = input("Do you still like roblox? y/n").lower() # Ask the user if they still like Roblox and check for response
   if user_input == "n" or user_input == "no": # Program exits the loop if the user enters either "no" or "n"
     dog = False # Ends the loop if the answer is no
+print("Connected to server")
