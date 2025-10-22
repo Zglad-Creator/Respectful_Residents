27a80f29530880e10dbf6e1af3af31c689fe09b1
Vincent Van Potent <harshithakash945@gmail.com>
2025-10-22 16:23:49 -0700

    Update exclude.py

--- exclude.py
+++ exclude.py
@@ -1,16 +1,5 @@
 import random
 
-phone = True  
-while phone:  
-    user_input = input("True or false: Thienlac is cool: ").lower()
-    if user_input in ["true", "t"]: 
-        print("That's what I thought.")  
-        phone = False  
-    elif user_input in ["false", "f"]:
-        print("No, Thienlac is cool.")
-    else:
-        print("You entered an incorrect answer; please answer with true or false")
-
 dog = True
 like_count = 0  
 roblox = 100000
