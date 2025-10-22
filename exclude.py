466cdefadd93dd07382b982ec49097cb6907a593
TheBigTL <thienlac1234@outlook.com>
2025-10-22 07:52:40 -0700

    Update exclude.py
    Added an elif and else statement to check for any cases where the user inputs an incorrect input. Also used .lower() to convert all uppercase letters from their input to lowercase letters. Also compares user_input to a list with all accepted inputs.

--- exclude.py
+++ exclude.py
@@ -1,3 +1,13 @@
+phone =True  
+while phone:  
+    user_input =input("True or false: Thienlac is cool: ").lower()
+    if user_input in ["true", "t"]: 
+        print("That's what I thought.")  
+        phone = False  
+    elif user_input in ["false", "f"]:
+        print("No, Thienlac is cool.")
+    else:
+        print("You entered an incorrect answer; please answer with true or false")
 dog = True
 like_count = 0  # Initialize a counter to track how many times the user says "yes"
 roblox = 100000
